0710b9930689b683a9abfcf30be36903bd42d0c8
Mike Lynch <nycynik@gmail.com>
2019-05-03 17:52:40 -0700

    Added a simple healtcheck, should move this to another class shortly.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,14 +7,33 @@ public func routes(_ router: Router) throws {
         return "It works!"
     }
     
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    // Add basic healthcheck
+    router.get("healthcheck") { req -> HealthCheckResponse in
+
+        let serviceTests = HealthCheckServices(name: "PostgreSQL", status: "Operational");
+        let healthCheck: HealthCheckResponse! = HealthCheckResponse(generated_at: Date()
+            , tests: [serviceTests])
+        return healthCheck
+    }
+    
+    router.get("healthcheck", "ping") { req in
+        return "pong";
     }
 
+    
     // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    let todoController = TodoController();
+    router.get("todos", use: todoController.index);
+    router.post("todos", use: todoController.create);
+    router.delete("todos", Todo.parameter, use: todoController.delete);
+}
+
+struct HealthCheckResponse: Content {
+    var generated_at: Date = Date();
+    var tests: Array<HealthCheckServices>;
+}
+
+struct HealthCheckServices: Content {
+    var name: String;
+    var status: String;
 }
